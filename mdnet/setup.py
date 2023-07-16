72e5e154c4cf3b97395bd21f27e7d94de83c6a3d
N.A. Trimmer <nicholasatrimmer@gmail.com>
2023-07-16 04:09:58 -0700

    Updated docs and code readability

--- mdnet/setup.py
+++ mdnet/setup.py
@@ -1,17 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-        name = "mdnet",
-        version = "0.1",
-        packages = find_packages(),
-        entry_points = {
-            "console-scripts": [
-                "mdnet = mdnet:main",
-            ],
-        },
-        install_requires = [
-            "markdown",
-            "jinja2",
-            "python-frontmatter",
+    name='mdnet',
+    version='0.1.0',
+    packages=['mdnet'],
+    install_requires=[
+        'markdown',
+        'jinja2',
+        'python-frontmatter'
+    ],
+    entry_points={
+        'console_scripts': [
+            'mdnet=mdnet.mdnet:main',
         ],
+    },
 )
